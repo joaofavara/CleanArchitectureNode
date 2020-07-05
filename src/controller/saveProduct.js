ed20467bb35fda0ccd9fb2983010244ad3c7a932
Joao Pedro Favara <joaopedro_favara@hotmail.com>
2020-07-05 09:58:11 -0300

    fix: fix the return result from the controller

--- src/controller/saveProduct.js
+++ src/controller/saveProduct.js
@@ -2,10 +2,11 @@ module.exports = function makePostProduct(saveProduct) {
     return async function postProduct (req, res) {
         try {
             const body = req.body;
-            const result = await saveProduct(body)
-            res.send(200, result);
+            const result = await saveProduct(body);
+            res.status(200).send(result);
         } catch(error) {
             console.log(error);
+            res.status(404).send(error);
         }
     }
 }
